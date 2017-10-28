1eaadbe04154da5230726edf578965de23d77f1f
ggbmo <ggbmo@users.noreply.github.com>
2017-10-28 18:17:29 -0500

    script for moveForward

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import video_dir
+import motor
+import car_dir
+from socket import *
+from time import ctime          # Import necessary modules   
+
+ctrl_cmd = ['forward', 'backward', 'left', 'right', 'stop', 'read cpu_temp', 'home', 'distance', 'x+', 'x-', 'y+', 'y-', 'xy_home']
+
+busnum = 1          # Edit busnum to 0, if you uses Raspberry Pi 1 or 0
+
+video_dir.setup(busnum=busnum)
+car_dir.setup(busnum=busnum)
+motor.setup(busnum=busnum)     # Initialize the Raspberry Pi GPIO connected to the DC motor. 
+video_dir.home_x_y()
+car_dir.home()
+
+while True:
+    motor.forwardWithSpeed()
