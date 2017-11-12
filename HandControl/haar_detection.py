f6dd832628b1d1193005d1d8e2b46f0e26f21248
Gabriel Goetten <gabrielgoetten@cometnet-10-21-19-41.utdallas.edu>
2017-11-12 13:20:32 -0600

    initial commit of hand controlled logic

---
+++ HandControl/haar_detection.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2
+import matplotlib as mp
+import motor
+import car_dir as turn
+import time
+
+cap = cv2.VideoCapture(0)
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+motor.setup()
+turn.setup()
+turn.home()
+
+while True:
+	ret, frame = cap.read()
+	
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	
+	faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+	
+	for (x, y, w, h) in faces:
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        if x < 215:
+            turn.turn_left()
+            motor.forward_with_speed()
+            time.sleep(1)
+            motor.stop()
+            turn.home()
+        elif x > 215 && x < 430:
+            motor.forward_with_speed()
+            time.sleep(1)
+            motor.stop()
+        else:
+            turn.turn_right()
+            motor.forward_with_speed()
+            time.sleep(1)
+            motor.stop()
+            turn.home()
+
+	cv2.imshow('frame',frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
