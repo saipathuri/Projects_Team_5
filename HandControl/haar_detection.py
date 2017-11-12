5868225ede4fe1dd9b0dafdfa3a4981e459322d4
MananManan <man9501@gmail.com>
2017-11-12 16:56:59 -0600

    Change "face" to "hand"

--- HandControl/haar_detection.py
+++ HandControl/haar_detection.py
@@ -8,7 +8,7 @@ from threading import Thread
 
 cap = cv2.VideoCapture(0)
 cascPath = "closed_frontal_palm.xml"
-faceCascade = cv2.CascadeClassifier(cascPath)
+handCascade = cv2.CascadeClassifier(cascPath)
 
 motor.setup()
 turn.setup()
@@ -44,9 +44,9 @@ while True:
 
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+	hands = handCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-	for (x, y, w, h) in faces:
+	for (x, y, w, h) in hands:
 		cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 		if x < 215:
 			Thread(target = left_forward).start()
