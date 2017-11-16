838140be15e555d7b6b67154b7b8c02458995113
ggbmo <ggbmo@users.noreply.github.com>
2017-11-15 21:30:05 -0600

    Add files via upload

---
+++ haar_cascade2.py
@@ -0,0 +1,104 @@
+import numpy as np
+import cv2
+import matplotlib as mp
+import motor
+import car_dir as turn
+import time
+import video_dir as camTurn
+from threading import Thread
+
+cap = cv2.VideoCapture(0)
+cascPath = "closed_frontal_palm.xml"
+handCascade = cv2.CascadeClassifier(cascPath)
+
+motor.setup()
+turn.setup()
+turn.home()
+
+def forward():
+	turn.home()
+	print("go forward")
+	motor.forwardWithSpeed(spd=100)
+	time.sleep(1)
+	motor.stop()
+
+def right_forward():
+	turn.home()
+	print("turn right")
+	turn.turn_right()
+	motor.forwardWithSpeed(spd=100)
+	time.sleep(1)
+	motor.stop()
+	turn.home()
+
+def left_forward():
+	print("turn left")
+	turn.home()
+	turn.turn_left()
+	motor.forwardWithSpeed(spd=100)
+	time.sleep(1)
+	motor.stop()
+	turn.home()
+
+def count_frames_manual(cap):
+    # initialize the total number of frames read
+    total = 0
+
+    # loop over the frames of the video
+    while True:
+        # grab the current frame
+        (grabbed, frame) = cap.read()
+
+        # check to see if we have reached the end of the
+        # video
+        if not grabbed:
+            break
+
+        # increment the total number of frames read
+        total += 1
+
+    # return the total number of frames in the video file
+    return total
+
+while True:
+	ret, frame = cap.read()
+
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	hands = handCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    num_hands = len(hands)
+    total = count_frames_manual(cap)
+
+    if total > 15
+        total = 0
+        camTurn.Current_x = 0
+        pwm.write(14,0,camTurn.Current_x)
+
+        while camTurn.Current_x < 640 or num_hands == 0
+            Thread(target = move_increase_x).start()
+            hands
+            num_hands
+
+
+	for (x, y, w, h) in hands:
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+		if x > 215 and x < 430
+            if camTurn.Current_x < 295
+                Thread(target = left_forward).start()
+			elif camTurn.Current_x > 345
+                Thread(target = right_forward).start()
+            else:
+                Thread(target = forward).start()
+		else:
+			if x < 215:
+				Thread(target = move_decrease_x).start()
+			else:
+				Thread(target = move_increase_x).start()
+
+	cv2.imshow('frame',frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
